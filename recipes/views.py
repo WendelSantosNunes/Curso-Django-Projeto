ec9fc4082122477692065ba2eb65f38594d017ee
WendelSantosNunes <wendelnunes9999@gmail.com>
2022-06-30 10:59:35 -0300

    Update project

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Wendel',
+    })
+
+
+def contato(request):
+    return render(request, 'recipes/contato.html')
+
+
+def sobre(request):
+    return HttpResponse('Sobre 2')
